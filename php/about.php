0bf2f00355845591cf49671111316d9437638aaa
Arthaloith <nguyenmanhhung1904@gmail.com>
2023-11-02 17:47:25 +0700

    added about page

---
+++ php/about.php
@@ -0,0 +1,88 @@
+<?php
+    include_once './template/header.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>amogus store</title>
+    <link rel="stylesheet" type="text/css" href="../css/template/base.css">
+    <link rel="stylesheet" type="text/css" href="../css/template/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/template/footer.css">
+    <link rel="stylesheet" type="text/css" href="../css/home.css">
+    <link rel="stylesheet" type="text/css" href="../css/about.css">
+    <script src="js/loginpanel.js"></script>
+    <script src="https://kit.fontawesome.com/f1e51c2d13.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <section id="page-header" class="about-header">
+    </section>
+
+    <section id="about-head" class="section-p1">
+        <img src="../assets/unknown.jpg" alt="">
+        <div>
+            <h2>Who are we?</h2>
+            <p>
+                We are a group of individuals driven by a common passion for excellence and innovation. Our team is comprised of professionals from diverse backgrounds, each bringing their unique skills and expertise to the table. Together, we form a cohesive unit dedicated to delivering exceptional products and services to our clients.
+
+                Sus - Our Inspiration and Guiding Principle
+
+                Sus represents our core philosophy and guiding principle. It embodies the essence of our approach and sets us apart in the industry. Sus is a term that encompasses the combination of trust, reliability, and forward-thinking. It represents our commitment to providing the highest quality solutions that exceed expectations.
+
+                At our core, we believe in fostering long-lasting partnerships with our clients, built on a foundation of trust and transparency. We strive to establish ourselves as a reliable and dependable resource, consistently delivering results that drive success.
+
+                Our Mission
+
+                Our mission is to empower individuals and businesses with cutting-edge solutions that enable growth and success. We are committed to leveraging the latest technologies and industry best practices to deliver innovative and tailored solutions to our clients.
+
+                Our Vision
+
+                Our vision is to be a leading provider of transformative solutions, recognized for our unwavering commitment to excellence. We aim to be at the forefront of technological advancements, continuously evolving and adapting to meet the changing needs of our clients and the market.
+
+                With a customer-centric approach and a passion for continuous improvement, we are dedicated to helping our clients achieve their goals and surpass their expectations.
+
+                Join us on this journey as we strive to make a difference and leave a lasting impact in the ever-evolving world of technology and innovation. Together, we can unlock new possibilities and shape a brighter future.
+            </p>
+        </div>
+    </section>
+
+    <section class="credit">
+    <h2>Team Members</h2>
+
+    <div class="container">
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Worked On</th>
+            </tr>
+            <tr>
+                <td>N/A</td>
+                <td>Huynh Thanh Phat</td>
+                <td>ToS, Policy, Adding Image and Wrote Paragraphs</td>
+            </tr>
+            <tr>
+                <td>207CT40291</td>
+                <td>Nguyen Manh Hung</td>
+                <td>Everything Else</td>
+            </tr>
+        </table>
+    </div>
+    
+    </section>
+    <br>
+    <br>
+    <br>
+</body>
+</html>
+
+<?php
+    include_once './template/newsletter.php'
+?>
+
+
+<?php
+    include_once './template/footer.php'
+?>
