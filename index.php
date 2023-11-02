64b7e8d338c8a476615ae9b7d9e4282cc4925cb6
Arthaloith <nguyenmanhhung1904@gmail.com>
2023-11-02 12:29:47 +0700

    some small stuffs as per usual

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php
-    session_start();
-?>
 
+
+<?php
+    include_once './php/header.php'
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,27 +17,7 @@
     <script src="https://kit.fontawesome.com/f1e51c2d13.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <section class="header">
-        <a href="#" class="logo"><img src="assets/sus.jpg" alt="" style="width: 50px; height: 40px;"></a>
-        <div>
-            <ul class="navbar">
-                <li><a href="index.php" class="active">Home</a></li>
-                <li><a href="product.html">Products</a></li>
-                <li><a href="news.html">News</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="cart.html"><i class="fa-solid fa-bag-shopping"></i></a></li>
-                <?php
-                    if (isset($_SESSION["userid"])) {
-                        echo "<li><a href='incl/profile.php'>Profile</a></li>";
-                        echo "<li><a href='incl/logout.php'>Logout</a></li>"; 
-                    } 
-                ?>
-                <li><a href="signup.php"><i class="fa-solid fa-user-plus"></i></a></li>
-                <li><a href="login.php"><i class="fa-solid fa-right-to-bracket"></i></a></li>
-            </ul>
-        </div>
-    </section>
+    
     
     <section class="home">
         <h4>Help Jerma settle his debt.</h4>
